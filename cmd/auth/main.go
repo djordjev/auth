8ecf0645c121a82a4ecc60237f5ae814e1a52554
Djordje Vukovic <djordje@gyorgy.io>
2023-09-22 11:21:26 +0200

    Created package

--- cmd/auth/main.go
+++ cmd/auth/main.go
@@ -4,61 +4,36 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/djordjev/auth/internal/api"
-	"github.com/djordjev/auth/internal/domain"
 	"github.com/djordjev/auth/internal/models"
-	"github.com/djordjev/auth/internal/notify"
 	"github.com/djordjev/auth/internal/utils"
-	"github.com/redis/go-redis/v9"
+	"github.com/djordjev/auth/packages/server"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func main() {
+	// Read config
 	config, err := utils.BuildConfigFromEnv()
 	if err != nil {
 		panic(err)
 	}
 
-	r := http.NewServeMux()
-
-	// Init database
+	// Auto migrate database
 	dbUrl := config.GetConnectionString()
-	db, err := gorm.Open(postgres.Open(dbUrl), &gorm.Config{SkipDefaultTransaction: true})
+	db, err := gorm.Open(postgres.Open(dbUrl), &gorm.Config{})
 	if err != nil {
 		panic(err)
 	}
 
-	if err = models.AutoMigrate(db); err != nil {
+	if err := models.AutoMigrate(db); err != nil {
 		panic(err)
 	}
 
-	// Init redis
-	client := redis.NewClient(&redis.Options{
-		Addr:     fmt.Sprintf("%s:%d", config.RedisHost, config.RedisPort),
-		Password: config.RedisPassword,
-		DB:       config.RedisDatabase,
-	})
-
-	repo := models.NewRepository(db, client)
-
-	// Init api
-	logger := utils.MustBuildLogger(config)
-	var notifier domain.Notifier
-	if config.IsDev() {
-		notifier = notify.SilentNotifier{}
-	} else {
-		notifier = notify.NewMailjetNotifier(config)
-	}
-
-	// Init app domain
-	appDomain := domain.NewDomain(repo, config, notifier)
-
-	appApi := api.NewApi(config, r, appDomain, logger)
-
 	// Start up
-	appApi.Mount("/")
+	r := http.NewServeMux()
+	server := server.NewServer(r, config)
+	server.Mount("/")
 
-	fmt.Println("running")
+	fmt.Printf("Running server on port %s\n", config.Port)
 	http.ListenAndServe(fmt.Sprintf(":%s", config.Port), r)
 }
--- packages/server/server.go
+++ packages/server/server.go
@@ -1,17 +1,68 @@
 package server
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/djordjev/auth/internal/api"
+	"github.com/djordjev/auth/internal/domain"
+	"github.com/djordjev/auth/internal/models"
+	"github.com/djordjev/auth/internal/notify"
+	"github.com/djordjev/auth/internal/utils"
+	"github.com/redis/go-redis/v9"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
 
 type server struct {
-	mux *http.ServeMux
+	mux    *http.ServeMux
+	api    api.Api
+	config utils.Config
 }
 
 func (s *server) Mount(url string) {
-	s.mux.Handle(url, nil)
+	s.api.Mount(url)
 }
 
-func NewServer(mux *http.ServeMux) *server {
-	srv := &server{mux: mux}
+func NewServer(mux *http.ServeMux, config utils.Config) *server {
+	srv := &server{mux: mux, config: config}
+
+	srv.setup()
 
 	return srv
+}
+
+func (s *server) setup() {
+	// Init database
+	dbUrl := s.config.GetConnectionString()
+	db, err := gorm.Open(postgres.Open(dbUrl), &gorm.Config{SkipDefaultTransaction: true})
+	if err != nil {
+		panic(err)
+	}
+
+	// Init redis
+	client := redis.NewClient(&redis.Options{
+		Addr:     fmt.Sprintf("%s:%d", s.config.RedisHost, s.config.RedisPort),
+		Password: s.config.RedisPassword,
+		DB:       s.config.RedisDatabase,
+	})
+
+	// Setup repos
+	repo := models.NewRepository(db, client)
+
+	// Init api
+	logger := utils.MustBuildLogger(s.config)
+	var notifier domain.Notifier
+	if s.config.IsDev() {
+		notifier = notify.SilentNotifier{}
+	} else {
+		notifier = notify.NewMailjetNotifier(s.config)
+	}
+
+	// Init app domain
+	appDomain := domain.NewDomain(repo, s.config, notifier)
+
+	// Init API
+	appApi := api.NewApi(s.config, s.mux, appDomain, logger)
+	s.api = appApi
 }
