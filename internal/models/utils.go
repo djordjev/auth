62125644c830f1cd09aa75f646fb0f3f1ad3ca60
Djordje Vukovic <djordje@gyorgy.io>
2023-10-06 12:37:51 +0200

    Update migrations url

--- internal/models/utils.go
+++ internal/models/utils.go
@@ -1,24 +1,52 @@
 package models
 
 import (
-	"fmt"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
 
 	"github.com/djordjev/pg-mig/migrations"
 )
 
 func AutoMigrate(connectionString string) error {
+	envMigrations := os.Getenv("TEST_DB_MIGRATIONS")
+	wd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
 
-	fmt.Println(connectionString)
+	var migrationsFolder string
+	if envMigrations != "" {
+		migrationsFolder = envMigrations
+	} else {
+		migrationsFolder = filepath.Join(wd, "migrations")
+	}
+
+	trimProtocol := strings.TrimPrefix(connectionString, "postgres://")
+
+	credentialsArr := strings.Split(trimProtocol, "@")
+	credentials := credentialsArr[0]
+
+	dbInfo := strings.Split(credentialsArr[1], "/")
+	dbName := dbInfo[1]
+
+	hostPort := strings.Split(dbInfo[0], ":")
+
+	host := hostPort[0]
+
+	port, err := strconv.Atoi(hostPort[1])
+	if err != nil {
+		return err
+	}
 
 	runner := migrations.NewRunner(
-		"localhost",
-		"tester:testee",
-		"testdb",
-		5433,
-		"/Users/djordjev/Documents/Projects/auth/migrations",
+		host,
+		credentials,
+		dbName,
+		port,
+		migrationsFolder,
 	)
 
-	err := runner.Run([]string{})
-
-	return err
+	return runner.Run([]string{})
 }
